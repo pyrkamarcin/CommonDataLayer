994932127d9db68294d7e65ece3efcadd9eea4cf
luke.biel <lukasz.biel@pl.abb.com>
2020-11-25 16:30:47 +0100

    fix(data-router): change singular input-addr env to rabbitmq_username, password, etc (#37)
    We are usually provided with separate envs instead of one amqp address - thus the change

--- data-router/src/main.rs
+++ data-router/src/main.rs
@@ -9,6 +9,7 @@ use std::{
     sync::{Arc, Mutex},
     time::{SystemTime, UNIX_EPOCH},
 };
+use structopt::StructOpt;
 use tokio::pin;
 use utils::{
     abort_on_poison,
@@ -23,25 +24,46 @@ use uuid::Uuid;
 const SERVICE_NAME: &str = "data-router";
 const RABBIT_CONSUMER_TAG: &str = "CDL_DATA_ROUTER";
 
-#[derive(Deserialize, Debug, Serialize)]
+#[derive(StructOpt, Deserialize, Debug, Serialize)]
 struct Config {
-    pub input_addr: String,
+    #[structopt(long, env)]
+    pub rabbitmq_username: String,
+    #[structopt(long, env)]
+    pub rabbitmq_password: String,
+    #[structopt(long, env)]
+    pub rabbitmq_host: String,
+    #[structopt(long, env)]
+    pub rabbitmq_port: String,
+    #[structopt(long, env)]
     pub input_queue: String,
+    #[structopt(long, env)]
     pub kafka_brokers: String,
+    #[structopt(long, env)]
     pub kafka_error_channel: String,
+    #[structopt(long, env)]
     pub schema_registry_addr: String,
+    #[structopt(long, env)]
     pub cache_capacity: usize,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    let config = Arc::new(envy::from_env::<Config>().context("Env vars not set correctly")?);
+    let config: Config = Config::from_args();
     metrics::serve();
 
-    let consumer =
-        CommonConsumer::new_rabbit(&config.input_addr, RABBIT_CONSUMER_TAG, &config.input_queue)
-            .await?;
+    let consumer = CommonConsumer::new_rabbit(
+        &format!(
+            "amqp://{}:{}@{}:{}/%2f",
+            config.rabbitmq_username,
+            config.rabbitmq_password,
+            config.rabbitmq_host,
+            config.rabbitmq_port
+        ),
+        RABBIT_CONSUMER_TAG,
+        &config.input_queue,
+    )
+    .await?;
     let producer = Arc::new(
         CommonPublisher::new_kafka(&config.kafka_brokers)
             .await
@@ -51,6 +73,10 @@ async fn main() -> anyhow::Result<()> {
     let consumer = consumer.leak();
     let message_stream = consumer.consume().await;
     pin!(message_stream);
+
+    let kafka_error_channel = Arc::new(config.kafka_error_channel);
+    let schema_registry_addr = Arc::new(config.schema_registry_addr);
+
     while let Some(message) = message_stream.next().await {
         match message {
             Ok(message) => {
@@ -58,7 +84,8 @@ async fn main() -> anyhow::Result<()> {
                     message,
                     cache.clone(),
                     producer.clone(),
-                    config.clone(),
+                    kafka_error_channel.clone(),
+                    schema_registry_addr.clone(),
                 ));
             }
             Err(error) => {
@@ -74,7 +101,8 @@ async fn handle_message(
     message: Box<dyn CommunicationMessage>,
     cache: Arc<Mutex<LruCache<Uuid, String>>>,
     producer: Arc<CommonPublisher>,
-    config: Arc<Config>,
+    kafka_error_channel: Arc<String>,
+    schema_registry_addr: Arc<String>,
 ) {
     let result: anyhow::Result<()> = async {
         let event: DataRouterInputData =
@@ -82,7 +110,7 @@ async fn handle_message(
         let since_the_epoch = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards"); // TODO: Ordering can be different when scaling
-        let topic_name = get_schema_topic(&cache, &event, &config.schema_registry_addr).await?;
+        let topic_name = get_schema_topic(&cache, &event, &schema_registry_addr).await?;
         let data = CommandServiceInsertMessage {
             object_id: event.object_id,
             payload: event.data,
@@ -102,7 +130,7 @@ async fn handle_message(
         error!("{:?}", error);
         send_message(
             producer.as_ref(),
-            &config.kafka_error_channel,
+            &kafka_error_channel,
             SERVICE_NAME,
             format!("{:?}", error).into(),
         )
