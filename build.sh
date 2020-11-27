fb6f4e357d94703932738b05df112a3a37bdfd57
luke.biel <lukasz.biel@pl.abb.com>
2020-11-27 11:00:31 +0100

    fix(build): fix naming of crates in build.sh (#46)

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ set -ex
 # CDL_VERSION=0.1.0
 # CDL_PUSH=true
 
-array=( data_router command_service query_router query_service leader_elector schema_registry document_storage upload_to_kafka upload_to_rabbitmq )
+array=( data-router command-service query-router query-service leader-elector schema-registry )
 DOCKER_BUILDKIT=1
 for i in "${array[@]}"
 do
-	docker build -t ${CDL_REPOSITORY_PATH}cdl_${i}:${CDL_VERSION:-latest} --build-arg BIN=${i} --build-arg ENV=${ENV:-PROD} .
+	docker build -t ${CDL_REPOSITORY_PATH}cdl-${i}:${CDL_VERSION:-latest} --build-arg BIN=${i} --build-arg ENV=${ENV:-PROD} .
 done
 
-if [[ ! -z "$CDL_PUSH" ]] 
+if [[ ! -z "$CDL_PUSH" ]]
 then
 	for i in "${array[@]}"
 	do
-		docker push ${CDL_REPOSITORY_PATH}cdl_${i}:${CDL_VERSION:-latest}
+		docker push ${CDL_REPOSITORY_PATH}cdl-${i}:${CDL_VERSION:-latest}
 	done
 fi
