17fdc5d48f27b18596f320eaf6e2bd0663965bc2
Wojciech Polak <wojciech.polak@hitachi-powergrids.com>
2020-12-01 17:29:12 +0100

    fix(command-service): Add more details to error (#64)

--- command-service/src/report/error.rs
+++ command-service/src/report/error.rs
@@ -5,7 +5,7 @@ use utils::messaging_system::CommunicationError;
 pub enum Error {
     #[error("Failed to create producer `{0}`")]
     ProducerCreation(CommunicationError),
-    #[error("Failed to deliver Kafka report")]
+    #[error("Failed to deliver Kafka report `{0}`")]
     FailedToReport(CommunicationError),
     #[error("Failed to produce error message `{0}`")]
     FailedToProduceErrorMessage(serde_json::Error),
