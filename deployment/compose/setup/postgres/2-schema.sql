bb224b56db54fd32f603f972d3576448de9a2f5b
Wojciech Polak <wojciech.polak@hitachi-powergrids.com>
2021-05-12 13:31:33 +0200

    fix: schema registry docker-compose definitions and setup (#475)

--- deployment/compose/setup/postgres/2-schema.sql
+++ deployment/compose/setup/postgres/2-schema.sql
@@ -25,12 +25,10 @@ CREATE TABLE views (
     materializer_address varchar not null,
     materializer_options json not null,
     fields               json not null,
-    base_schema          uuid not null,
-    relations            json not null,
-    filters              json not null,
+    schema               uuid not null,
 
-    CONSTRAINT fk_base_schema_1
-        FOREIGN KEY(base_schema)
+    CONSTRAINT fk_schema_1
+        FOREIGN KEY(schema)
         REFERENCES schemas(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
