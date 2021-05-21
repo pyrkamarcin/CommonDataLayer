ab4ea52a300c22b3854f015f8f676a58bd49fbf1
Wojciech Polak <wojciech.polak@hitachi-powergrids.com>
2021-05-21 10:57:43 +0200

    fix: Add view to schema inserts all required columns (#502)

--- deployment/compose/setup.sh
+++ deployment/compose/setup.sh
@@ -12,32 +12,30 @@ fi
 
 echo "Starting $COMMUNICATION_METHOD + $QS_TYPE:$REPOSITORY_KIND"
 
-sed -i '' -E "s|communication_method = .+|communication_method = \"$COMMUNICATION_METHOD\"|g" setup/configuration/default.toml
-sed -i '' -E "s|repository_kind = .+|repository_kind = \"$REPOSITORY_KIND\"|g" setup/configuration/default.toml
+sed -i -E "s|communication_method = .+|communication_method = \"$COMMUNICATION_METHOD\"|g" setup/configuration/default.toml
+sed -i -E "s|repository_kind = .+|repository_kind = \"$REPOSITORY_KIND\"|g" setup/configuration/default.toml
 
 docker-compose down --remove-orphans
-docker-compose -f docker-compose.yml up -d postgres jaeger
+docker-compose up -d postgres jaeger
 
 sleep 5s
 
 if [ "$COMMUNICATION_METHOD" == "kafka" ]; then
-  docker-compose -f docker-compose.yml up -d kafka
+  docker-compose up -d kafka
 elif [ "$COMMUNICATION_METHOD" == "amqp" ]; then
-  docker-compose -f docker-compose.yml up -d rabbitmq
+  docker-compose up -d rabbitmq
 fi
 
 if [ "$REPOSITORY_KIND" == "victoria_metrics" ]; then
-  docker-compose -f docker-compose.yml up -d victoria_metrics
+  docker-compose up -d victoria_metrics
 elif [ "$REPOSITORY_KIND" == "druid" ]; then
-  docker-compose -f docker-compose.yml up -d \
+  docker-compose up -d \
     druid_coordinator druid_broker druid_historical druid_middlemanager druid_router
 fi
 
-sleep 5s
+sleep 15s
 
-docker-compose \
-  -f docker-compose.yml \
-  up -d \
+docker-compose up -d \
   schema_registry \
   command_service \
   web_api \
@@ -45,13 +43,13 @@ docker-compose \
   data_router
 
 if [ $QS_TYPE == "document" ]; then
-  docker-compose -f docker-compose.yml up -d query_service
+  docker-compose up -d query_service
 else
-  docker-compose -f docker-compose.yml up -d query_service_ts
+  docker-compose up -d query_service_ts
 fi
 
 if [ "$COMMUNICATION_METHOD" == "kafka" ]; then
-  docker-compose -f docker-compose.yml up -d \
+  docker-compose up -d \
     object_builder \
     materializer_ondemand \
     materializer_general
