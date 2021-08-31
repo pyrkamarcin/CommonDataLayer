85a82025d424c570320964fb7a6112ce4fc9d375
luke.biel <lukasz.biel@pl.abb.com>
2021-08-31 10:16:10 +0200

    test: materializer-general : elasticsearch support (#728)
    chore: add helm charts for materializer-general

---
+++ CDL.Tests/Utils/ElasticsearchConnector.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nest;
+
+namespace CDL.Tests.Utils
+{
+    public static class ElasticsearchConnector
+    {
+        public static IReadOnlyCollection<TDocument> QueryAll<TDocument>(Uri nodeUrl, string index) where TDocument : class
+        {
+            var settings = new ConnectionSettings(nodeUrl);
+            var client = new ElasticClient(settings);
+
+            client.Indices.Refresh(Indices.Index(index));
+
+            var res = client.Search<TDocument>(s => s.Index(index).MatchAll());
+            
+            return res.Documents;
+        }
+    }
+}
